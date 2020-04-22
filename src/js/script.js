d6ac01b2152df3d301acc6e9056233d59d828b03
Gosia Gossia <m.kudlak@icloud.com>
2020-04-22 22:46:44 +0200

    Updating files for project

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('hello')
+jQuery(document).ready(function (jQuery) {
+
+    $('.panel-title').click(function () {
+        $('.panel-title').removeClass('active');
+        $(this).addClass('active');
+        $('.panel-collapse').removeClass('show');
+        $(this).find('.panel-collapse').addClass('show');
+    });
+});
