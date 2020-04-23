176adac2bbccb518dabaf13af2b4f4172f9e1cbd
Gosia Gossia <m.kudlak@icloud.com>
2020-04-23 10:17:16 +0200

    Modified js- icons to change them when acc open

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,14 @@ jQuery(document).ready(function (jQuery) {
         $(this).addClass('active');
         $('.panel-collapse').removeClass('show');
         $(this).find('.panel-collapse').addClass('show');
+
     });
+
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).parent().find(".fas").removeClass("fa-chevron-circle-down").addClass("fa-chevron-circle-up");
+    }).on('hidden.bs.collapse', function () {
+        $(this).find(".fas").removeClass("fa-chevron-circle-up").addClass("fa-chevron-circle-down");
+    });
+
+
 });
